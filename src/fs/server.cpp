d2c2bacccee6796831c8a2c8d541d1ad5d79956e
praveenster <me@praveenster.com>
2013-09-24 21:42:37 -0700

    add stub implementation for tags and tag apis. these will later be replaced with db calls.

--- src/fs/server.cpp
+++ src/fs/server.cpp
@@ -6,15 +6,42 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <String.h>
 #include <vector>
 #include "microhttpd.h"
+#include "String.h"
+#include "JsonValue.h"
+#include "JsonObject.h"
+#include "JsonString.h"
+#include "JsonArray.h"
+#include "JsonNull.h"
+#include "JsonNumber.h"
+#include "SmartPointer.h"
 
 using std::vector;
+
+using lepcpplib::JsonValue;
+using lepcpplib::JsonObject;
+using lepcpplib::JsonString;
+using lepcpplib::JsonArray;
+using lepcpplib::JsonNull;
+using lepcpplib::JsonNumber;
+using lepcpplib::SmartPointer;
 using lepcpplib::String;
+
 const short int kPort = 8888;
 const int kFileBlockSize = 32 * 1024 * 1024;
 const String k404 = "<html><head><title>File not found</title></head><body>File not found</body></html>";
+
+int enqueue_404_response(MHD_Connection* connection)
+{
+    MHD_Response* response = MHD_create_response_from_buffer(k404.length(),
+      (void*)k404.toCharArray(), MHD_RESPMEM_PERSISTENT);
+
+    int result = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
+    MHD_destroy_response(response);
+
+    return result;
+}
 
 ssize_t file_server_callback(void *cls, uint64_t pos, char *buf, size_t max)
 {
@@ -40,9 +67,7 @@ int serve_file(MHD_Connection* connection, String& filename)
     result = MHD_queue_response(connection, MHD_HTTP_OK, response);
   }
   else {
-    response = MHD_create_response_from_buffer(k404.length(),
-      (void*)k404.toCharArray(), MHD_RESPMEM_PERSISTENT);
-    result = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
+    result = enqueue_404_response(connection);
   }
 
   MHD_destroy_response(response);
@@ -70,31 +95,7 @@ int route_discover(void* cls, MHD_Connection* connection,
     }
   }
 
-#if 0
-  FILE* fp = fopen(filename.toCharArray(), "r");
-  int fd = fileno(fp);
-  struct stat buf;
-  stat(filename.toCharArray(), &buf);
-
-  //response = MHD_create_response_from_buffer(output.length(),
-  //  (void*)output.toCharArray(), MHD_RESPMEM_MUST_COPY);
-  response = MHD_create_response_from_fd(buf.st_size, fd);
-
-  result = MHD_queue_response(connection, MHD_HTTP_OK, response);
-  MHD_destroy_response(response);
-  return result;
-#endif
   return serve_file(connection, filename);
-}
-
-int route_www(void* cls, MHD_Connection* connection,
-              const char* url,
-              const char* method, const char* version,
-              const char* upload_data,
-              size_t* upload_data_size, void** con_cls,
-              String& output)
-{
-  output = "<html><body>Hello, discoverer renderer caller.</body></html>";
 }
 
 int route_api(void* cls, MHD_Connection* connection,
@@ -102,9 +103,82 @@ int route_api(void* cls, MHD_Connection* connection,
               const char* method, const char* version,
               const char* upload_data,
               size_t* upload_data_size, void** con_cls,
-              String& output)
+              vector<String>& tokens)
 {
-  output = "<html><body>Hello, api caller.</body></html>";
+  SmartPointer<String> output;
+
+  int token_count = tokens.size();
+  if ((token_count >= 4) && (tokens[2] == "get") && (tokens[3] == "tags")) {
+    JsonObject* j1 = new JsonObject();
+    j1->Add(new JsonString("name"), new JsonString("root"));
+    j1->Add(new JsonString("id"), new JsonNumber(0));
+    j1->Add(new JsonString("parent"), new JsonNull());
+
+    JsonObject* j2 = new JsonObject();
+    j2->Add(new JsonString("name"), new JsonString("home"));
+    j2->Add(new JsonString("id"), new JsonNumber(1));
+    j2->Add(new JsonString("parent"), new JsonString("root"));
+
+    JsonArray* a = new JsonArray();
+    a->Add(j1);
+    a->Add(j2);
+
+    SmartPointer<JsonObject> j = new JsonObject();
+    j->Add(new JsonString("tags"), a);
+
+    output = j->ToString();
+  }
+  else if ((token_count >= 5) && (tokens[2] == "get") && (tokens[3] == "tag")) {
+    int tag_id = String::toInt(tokens[4].toCharArray());
+
+    if (tag_id == 0) {
+      JsonObject* j1 = new JsonObject();
+      j1->Add(new JsonString("name"), new JsonString("file1.txt"));
+      j1->Add(new JsonString("id"), new JsonNumber(0));
+
+      JsonObject* j2 = new JsonObject();
+      j2->Add(new JsonString("name"), new JsonString("file2.jpg"));
+      j2->Add(new JsonString("id"), new JsonNumber(1));
+
+      JsonArray* a = new JsonArray();
+      a->Add(j1);
+      a->Add(j2);
+
+      SmartPointer<JsonObject> j = new JsonObject();
+      j->Add(new JsonString("files"), a);
+      output = j->ToString();
+    }
+    else {
+      JsonObject* j1 = new JsonObject();
+      j1->Add(new JsonString("name"), new JsonString("file3.mp4"));
+      j1->Add(new JsonString("id"), new JsonNumber(2));
+
+      JsonObject* j2 = new JsonObject();
+      j2->Add(new JsonString("name"), new JsonString("file4.mp3"));
+      j2->Add(new JsonString("id"), new JsonNumber(3));
+
+      JsonArray* a = new JsonArray();
+      a->Add(j1);
+      a->Add(j2);
+
+      SmartPointer<JsonObject> j = new JsonObject();
+      j->Add(new JsonString("files"), a);
+      output = j->ToString();
+    }
+  }
+  else {
+    SmartPointer<JsonObject> j = new JsonObject();
+    j->Add(new JsonString("error"), new JsonString("invalid api"));
+
+    output = j->ToString();
+  }
+
+  MHD_Response* response = MHD_create_response_from_buffer(output->length(),
+    (void*)output->toCharArray(), MHD_RESPMEM_MUST_COPY);
+
+  int result = MHD_queue_response(connection, MHD_HTTP_OK, response);
+  MHD_destroy_response(response);
+  return result;
 }
 
 int client_processor(void* cls, MHD_Connection* connection,
@@ -115,40 +189,21 @@ int client_processor(void* cls, MHD_Connection* connection,
 {
   vector<String> tokens;
   String::tokenize(url, '/', tokens);
-  String output;
+  int result;
 
   if (tokens.size() >= 2) {
     if (tokens[1] == "discover") {
-      return route_discover(cls, connection, url, method, version,
+      result = route_discover(cls, connection, url, method, version,
         upload_data, upload_data_size, con_cls, tokens);
     }
     else if (tokens[1] == "api") {
       route_api(cls, connection, url, method, version,
-        upload_data, upload_data_size, con_cls, output);
-    }
-    else if (tokens[1] == "www") {
-      route_www(cls, connection, url, method, version,
-        upload_data, upload_data_size, con_cls, output);
+        upload_data, upload_data_size, con_cls, tokens);
     }
     else {
-      output = "<html><body>Hello, 404 found not.</body></html>";
+      result = enqueue_404_response(connection);
     }
   }
-
-  MHD_Response* response;
-  int result;
-  FILE* fp = fopen("../www/html/discover.html", "r");
-  int fd = fileno(fp);
-  struct stat buf;
-  stat("../www/html/discover.html", &buf);
-
-
-  //response = MHD_create_response_from_buffer(output.length(),
-  //  (void*)output.toCharArray(), MHD_RESPMEM_MUST_COPY);
-  response = MHD_create_response_from_fd(buf.st_size, fd);
-
-  result = MHD_queue_response(connection, MHD_HTTP_OK, response);
-  MHD_destroy_response(response);
 
   return result;
 }
